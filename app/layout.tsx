add556b3c15653d8b85ee6054bfea8d54727ac4a
Ermiyas Dagnachew <ermiyas.dagnachew@gmail.com>
2023-10-16 17:06:22 +0300

    Initial commit

---
+++ app/layout.tsx
@@ -0,0 +1,16 @@
+export const metadata = {
+  title: "New Dribble",
+  description: "A new dribble clone for the chinese market",
+};
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="en">
+      <body>{children}</body>
+    </html>
+  );
+}
---
+++ app/page.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const home = () => {
+  return (
+    <section className="flex-start padding">
+      <h1>Hello</h1>
+    </section>
+  );
+};
+
+export default home;
